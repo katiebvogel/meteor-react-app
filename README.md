e325571c9897711f9560703273f7c7738a285870
katiebvogel <katiebvogel@gmail.com>
2016-10-14 08:40:13 -0500

    updating

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 Basic Meteor app with some practice working with a new technology for me, React!
 
+https://www.meteor.com/tutorials/react/components
+
+
 after getting meteor started,
 run
 meteor npm install --save react react-dom
+
+
+
+To allow us to create a "data container" to feed Meteor's reactive data into React's component heierarchy:  
+meteor npm install --save react-addons-pure-render-mixin
+meteor add react-meteor-data
--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,8 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
 
+import { Tasks } from '../api/tasks.js';
 import Task from './Task.jsx';
 
 // App component - represents the whole app
+class App extends Component {
+renderTasks() {
+   return this.props.tasks.map((task) => (
+     <Task key={task._id} task={task} />
+   ));
+   }
+
 export default class App extends Component {
   getTasks() {
     return [
@@ -32,3 +41,13 @@ export default class App extends Component {
     );
   }
 }
+
+App.propTypes = {
+  tasks: PropTypes.array.isRequired,
+};
+
+export default createContainer(() => {
+  return {
+    tasks: Tasks.find({}).fetch(),
+  };
+}, App);
